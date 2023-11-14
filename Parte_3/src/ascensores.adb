e8158887090222396d93850b81f936bdef742f2a
Gonzalo Gambetta <gonzalogambetta2@gmail.com>
2023-11-14 11:47:26 -0300

    Arranco parte 3

---
+++ Parte_3/src/ascensores.adb
@@ -0,0 +1,14 @@
+with Ada.Text_IO; use ADA.Text_IO;
+with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
+with Ada.Command_Line;
+with Ada.Strings;
+with Ada.Task_Identification; use Ada.Task_Identification;
+
+procedure ascensores is 
+   -- cantidad de pedidos de 15
+   
+
+begin
+   delay 10.0;
+   null;
+end ascensores;
